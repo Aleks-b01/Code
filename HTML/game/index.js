eb1b425f1781679d26a94f5d53138c2646ace03c
Aleks <boberaleksander08@gmail.com>
2024-10-07 14:35:47 +0000

    update game

--- HTML/game/index.js
+++ HTML/game/index.js
@@ -4,6 +4,7 @@ document.addEventListener("keyup", handleStop);
 
 const body = document.getElementById("html");
 const player = document.getElementById("player");
+const pointer = document.getElementById("pointer");
 
 const height = screen.height;
 const width = screen.width;
@@ -20,6 +21,7 @@ let speed = setInterval(function() {
 	moveRight()
 	}
 	, 5);
+let aim = 1;
 
 function handleMovement(event) {
 	if ((event.key === "w" || event.key === "W")) {
@@ -38,6 +40,38 @@ function handleMovement(event) {
 		event.preventDefault();
 		d = true;
 		moveRight();
+	} else if (event.key === "ArrowUp" && (event.key !== "ArrowLeft" || event.key !== "ArrowRight")) {
+		event.preventDefault();
+		aim = 1;
+		aimPointer();
+	} else if (event.key === "ArrowDown" && (event.key !== "ArrowLeft" || event.key !== "ArrowRight")) {
+		event.preventDefault();
+		aim = 3;
+		aimPointer();
+	} else if (event.key === "ArrowLeft" && (event.key !== "ArrowUp" || event.key !== "ArrowDown")) {
+		event.preventDefault();
+		aim = 4;
+		aimPointer();
+	} else if (event.key === "ArrowRight" && (event.key !== "ArrowUp" || event.key !== "ArrowDown")) {
+		event.preventDefault();
+		aim = 2;
+		aimPointer();
+	} else if (event.key === "ArrowUp" && event.key === "ArrowRight") {
+		event.preventDefault();
+		aim = 5;
+		aimPointer();
+	} else if (event.key === "ArrowDown" && event.key === "ArrowRight") {
+		event.preventDefault();
+		aim = 6;
+		aimPointer();
+	} else if (event.key === "ArrowDown" && event.key === "ArrowLeft") {
+		event.preventDefault();
+		aim = 7;
+		aimPointer();
+	} else if (event.key === "ArrowUp" && event.key === "ArrowLeft") {
+		event.preventDefault();
+		aim = 8;
+		aimPointer();
 	}
 };
 
@@ -60,6 +94,7 @@ function moveUp() {
 	if (up > 0 && w == true) {
 		up -= 1;
 		player.style.top = up + "px";
+		aimPointer();
 	}
 };
 
@@ -67,6 +102,7 @@ function moveDown() {
 	if (up < height && s == true) {
 		up += 1;
 		player.style.top = up + "px";
+		aimPointer();
 	}
 };
 
@@ -74,6 +110,7 @@ function moveLeft() {
 	if (left > 0 && a == true) {
 		left -= 1;
 		player.style.left = left + "px";
+		aimPointer();
 	}
 };
 
@@ -81,6 +118,44 @@ function moveRight() {
 	if (left < width && d == true) {
 		left += 1;
 		player.style.left = left + "px";
+		aimPointer();
 	}
 };
+
+function aimPointer() {
+	if (aim == 1) {
+		pointer.style.rotate = "0deg";
+		pointer.style.top = (up - 50) + "px";
+		pointer.style.left = (left + 23) + "px";
+	} else if (aim == 2) {
+		pointer.style.rotate = "90deg";
+		pointer.style.top = (up + 15) + "px";
+		pointer.style.left = (left + 85) + "px";
+	} else if (aim == 3) {
+		pointer.style.rotate = "0deg";
+		pointer.style.top = (up + 80) + "px";
+		pointer.style.left = (left + 23) + "px";
+	} else if (aim == 4) {
+		pointer.style.rotate = "90deg";
+		pointer.style.top = (up + 15) + "px";
+		pointer.style.left = (left - 45) + "px";
+	} else if (aim == 5) {
+		pointer.style.rotate = "45deg";
+		pointer.style.top = (up - 50) + "px";
+		pointer.style.left = (left + 85) + "px";
+	}	else if (aim == 6) {
+		pointer.style.rotate = "135deg";
+		pointer.style.top = (up + 80) + "px";
+		pointer.style.left = (left + 85) + "px";
+	}	else if (aim == 7) {
+		pointer.style.rotate = "45deg";
+		pointer.style.top = (up + 80) + "px";
+		pointer.style.left = (left - 45) + "px";
+	} else if (aim == 8) {
+		pointer.style.rotate = "135deg";
+		pointer.style.top = (up - 50) + "px";
+		pointer.style.left = (left - 45) + "px";
+	}
+};
+
 });
