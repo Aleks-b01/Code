31f3bb1a8a095696853f6cffe15853652c560bc4
Aleks <boberaleksander08@gmail.com>
2024-10-15 08:52:29 +0200

    update

--- C/BetterCalc/bettercalc.c
+++ C/BetterCalc/bettercalc.c
@@ -2,18 +2,22 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 char userInput[];
-char num1[];
-char num2[];
+double num1;
+double num2;
+double result;
+char num1str[];
+char num2str[];
 char analyzer;
 char temp;
 int length;
+int length1 = 0;
+int length2 = 0;
 int operator = 0;
 int option;
 int inputCount = 0;
-bool continueInput = false;
-bool num1alr = false;
 
 void getUserInput() {
 	printf("\n");
@@ -24,47 +28,66 @@ void getUserInput() {
 
 void processUserInput() {
 	for (int i = length; i >= 0; i--) {
-		analyzer = userInput[i];
-		if (isdigit(analyzer) && num1alr == false) {
-			num2[inputCount] = analyzer;
-			inputCount++;
-		} else if ((analyzer == '.' || analyzer == ',') && num1alr == false) {
-			num2[inputCount] = '.';
-			inputCount++;
+		temp = userInput[i];
+		if (isdigit(temp) && (temp == '.' || temp == ',')) {
+			length2++;
 		} else {
-			num1alr = true;
-			inputCount = 0;
-		}
-		if (isdigit(analyzer) && num1alr == true) {
-			num1[inputCount] = analyzer;
-			inputCount++;
-		} else if ((analyzer == '.' || analyzer == ',') && num1alr == true) {
-			num1[inputCount] = '.';
-			inputCount++;
-		} else {
-			num1alr = false;
-			inputCount = 0;
-		}
-		if (analyzer == '+') {
-			operator = 1;
-			break;
-		} else if (analyzer == '-') {
-			operator3 = 2;
-			break;
-		} else if (analyzer == '*') {
-			operator = 3;
-			break;
-		} else if (analyzer == '/') {
-			operator = 4;
 			break;
 		}
 	}
-	processUserInput2();
+	for (int i = length - length2; i >= 0; i--) {
+		temp = userInput[i];
+		if (isdigit(temp) && (temp == '.' || temp == ',')) {
+			length1++;
+		}
+	}
+	for (int i = length; i >= 0; i--) {
+		temp = userInput[i];
+		if (isdigit(temp)) {
+			num2str[length2 - 1] = temp;
+			length2--;
+		} else if (temp == '.' || temp == ',') {
+			num2str[length2 - 1] = '.';
+			length2--;
+		} else {
+			break;
+		}
+	}
+	num2 = atof(num2str);
+	for (int i = length - length2; i >= 0; i--) {
+		temp = userInput[i];
+		if (isdigit(temp)) {
+			num1str[length1 - 1] = temp;
+			length1--;
+		} else if (temp == '.' || temp == ',') {
+			num1str[length1 - 1] = '.';
+			length1--;
+		}
+	}
+	num1 = atof(num1str);
+	for (int i = length; i > 0; i--) {
+		temp = userInput[i];
+		if (temp == '+') {
+			operator = 1;
+		} else if (temp == '-') {
+			operator = 2;
+		} else if (temp == '*') {
+			operator = 3;
+		} else if (temp == '/') {
+			operator = 4;
+		}
+	}
+	calculate();
 }
 
-void processUserInput2() {
-	for (int i = strlen(num1); i >= 0; i--) {
-		
+void calculate() {
+	if (operator == 1) {
+		result = num1 + num2;
+		printf("\n%.2f\n", result);
+	} else if (operator == 2) {
+		result = num1 - num2;
+		printf("\n%.2f\n", result);
+	}
 }
 
 int main() {
