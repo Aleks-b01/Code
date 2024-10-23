431d28e6324d6aa7eddbadd2bcadd9434ca7bbea
Aleks <boberaleksander08@gmail.com>
2024-10-23 15:30:17 +0200

    update

--- C/bank/bank.c
+++ C/bank/bank.c
@@ -6,67 +6,69 @@
 
 FILE *account;
 
+int choice;
 int passwdCount = 0;
-char passwd[];
-char passwd2[];
+char passwd[300];
+char passwd2[300];
 char accountName[16];
-char accounts[][16];
+char accounts[][16] = { {}, {} };
 
 int openPasswd() {
 	while(1) {
 		passwdCount = 0;
-		printf("\nEnter a new password:");
-		passwd.clear();
-		passwd2.clear();
-		scanf("\n%s\n", passwd);
-		printf("\nConfirm password:");
-		scanf("\n%s\n", passwd2);
+		printf("\nEnter a new password:\n");
+		strcpy(passwd, "");
+		strcpy(passwd2, "");
+		scanf("%s", passwd);
+		printf("\n\nConfirm password:\n");
+		scanf("%s", passwd2);
 		while(1) {
 			if (passwd == passwd2) {
 				return 0;
 			} else if (passwdCount < 3) {
-				printf("\nThe passwords don't match\nEnter the password again:");
-				passwd2.clear();
-				scanf("\n%s\n", passwd2);
+				printf("\n\nThe passwords don't match\nEnter the password again:\n");
+				strcpy(passwd2, "");
+				scanf("%s", passwd2);
 				passwdCount++;
 			} else {
 				passwdCount = 0;
 				return 0;
-		}
-		if (passwd == passwd2) {
+			}
+			if (passwd == passwd2) {
 			return 0;
+			}
 		}
-	}
 	passwdCount = 0;
 	return 0;
+	}
 }
 
 int openAcc() {
-	pritnf("\nEnter a account name (max. 16 characters):");
-	scanf("\n%s\n", accountName);
-	printf("\nEnter a password (at least 4 characters):");
-	scanf("\n%s\n", passwd);
-	if (passwd.length() < 4) {
+	printf("\n\nEnter a account name (max. 16 characters):\n");
+	scanf("%s", accountName);
+	printf("\n\nEnter a password (at least 4 characters):\n");
+	scanf("%s", passwd);
+	if (strlen(passwd) < 4) {
 		while(1) {
-			if (passwd.length() < 4) {
-				printf("\nThe password is too short\nEnter another password:");
-				passwd.clear();
-				scanf("\n%s\n", passwd);
+			if (strlen(passwd) < 4) {
+				printf("\n\nThe password is too short\nEnter another password:\n");
+				strcpy(passwd, "");
+				scanf("%s", passwd);
 			} else {
 				return 0;
 			}
 		}
 	}
-	printf("\nConfirm password:");
-	scanf("\n%s\n", passwd2);
+	printf("\n\nConfirm password:\n");
+	scanf("%s", passwd2);
 	if (passwd != passwd2) {
 		while (1) {
-			if (passwd != passwd2 && count < 3) {
-				printf("\nThe passwords don't match\nEnter the password again:");
-				passwd2.clear();
-				scanf("\n%s\n", passwd2);
-				count++;
-			} else if (passwd != passwd2 && count == 3) {
+			if (passwd != passwd2 && passwdCount < 3) {
+				printf("\n\nThe passwords don't match\nEnter the password again:\n");
+				strcpy(passwd2, "");
+				scanf("%s", passwd2);
+				passwdCount++;
+			} else if (passwd != passwd2 && passwdCount == 3) {
 				int openPasswd();
 				return 0;
 			} else {
@@ -80,7 +82,7 @@ int openAcc() {
 }
 
 int manage() {
-	pritnf("\n\n\nChoose an option:\n1. Deposit\n2. Withdraw\n3. ");
+	printf("\n\n\nChoose an option:\n1. Deposit\n2. Withdraw\n3. ");
 	return 0;
 }
 
@@ -93,12 +95,13 @@ int login() {
 int main() {
 	while (1) {
 		printf("\nWelcome to the Bank manager\n\n\n\nChoose an option:\n1. Manage account\n2. Open account\n3. Exit\n");
-		int choice;
-		scanf("\n%d\n", choice);
+		scanf("%d", choice);
 		switch(choice) {
-			case 1:			
+			case 1:	
+				manage();
 				break;
 			case 2:
+				openAcc();
 				break;
 			case 3:
 				return 0;
