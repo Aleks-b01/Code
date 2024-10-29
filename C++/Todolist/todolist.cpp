91a0c0eb1298f46312e887bde0a0748cc58ccf06
Aleks <boberaleksander08@gmail.com>
2024-10-29 11:14:15 +0100

    update

--- C++/Todolist/todolist.cpp
+++ C++/Todolist/todolist.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+int initChoice;
+
 int main() {
-	cout << "\n1. Add item\n2. Remove item\n3. Exit";
+	while(1){
+		cout << "\n1. Add item\n2. Remove item\n3. Exit\n";
+		cin >> initChoice;
+		switch(initChoice) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				return 0;
+			default:
+				cout << "\nInvalid Input\n";
+				break;
+		}
+	}
 	return 0;
 }
