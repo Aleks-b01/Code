3301831aac59e39dadcf1ed0fa06b4a6e94e4d08
Aleks <boberaleksander08@gmail.com>
2024-11-11 14:18:38 +0100

    update

--- HTML/mg/js/index.js
+++ HTML/mg/js/index.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener("keydown", keyBinds);
 
+const css = document.getElementsByTagName('link');
+
+const screenwidth = screen.width
+
+window.onload = function() {
+	if (screenwidth > 600) {
+		css[0].setAttribute('href', 'index.css');
+	} else if (screenwidth <= 600) {
+		css[0].setAttribute('href', 'mobile.css');
+	}
+};
+
 function keyBinds(event) {
 
 };
