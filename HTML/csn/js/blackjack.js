927da67e1151107cfdb5e546bf2df63bd69e090e
Aleks <boberaleksander08@gmail.com>
2025-01-22 16:43:18 +0100

    update

--- HTML/csn/js/blackjack.js
+++ HTML/csn/js/blackjack.js
@@ -3,12 +3,10 @@ document.addEventListener("keydown", keybinds);
 const blackjack = document.getElementById("blackjack");
 const chipsCounter = document.getElementById("chipsCounter");
 
+let chips = chipsCounter.innerText;
+
 function keybinds(event) {
 	if (event.key === "Escape") {
 		event.preventDefault();
 	}
-};
-
-blackjack.onclick = function() {
-	console.log(chipsCounter.innerText);
 };
